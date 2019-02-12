89804069d3e0c2f07e545e862c8926ee1bdcdadf
Jose Marcos Rodríguez Fernández <jose.marcos.rf@gmail.com>
2019-02-12 20:31:32 +0200

    small refactoring

--- pybpe/pybpe.py
+++ pybpe/pybpe.py
@@ -3,7 +3,7 @@ import typing
 import logging
 import coloredlogs
 
-from typing import Text
+from typing import Text, List, Tuple
 
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='INFO',
@@ -15,18 +15,43 @@ class pyBPE:
 
     @staticmethod
     def get_vocab(text: Text) -> List[Tuple[Text, int]]:
-        return bpe.get_vocabs(text)
+        try:
+            return bpe.get_vocabs(text)
+        except Exception as e:
+            logger.error("Unknown error while computing vocab: {}".format(e))
+            logger.exception(e)
 
     @staticmethod
     def get_bpe_codes(text: Text, n_codes: int) -> List[Tuple[Text, Text, int]]:
-        return bpe.learn_bpes(n_codes, text)
+        try:
+            return bpe.learn_bpes(n_codes, text)
+        except Exception as e:
+            logger.error("Unknown error while computing BPE codes: {}".format(e))
+            logger.exception(e)
 
     @staticmethod
     def create_vocab_file(text: Text, output_path: Text) -> None:
-        try:
-            vocab = bpe.get_vocabs(text)
+        vocab = pyBPE.get_vocab(text)
+        pyBPE.write_vocab_file(vocab, output_path)
 
-            # to write to file sorted by frequency
+    @staticmethod
+    def create_bpe_file(text: Text, n_codes: int, output_path: Text) -> None:
+        codes = pyBPE.get_bpe_codes(text, n_codes)
+        pyBPE.write_codes_file(codes, output_path)
+
+    @staticmethod
+    def apply_bpe(text: Text, codes_path: Text, vocab_path: Text) -> Text:
+        try:
+            return bpe.apply_bpes(text, codes_path, vocab_path)
+        except Exception as e:
+            logger.error("Unknown error while applying BPE codes file: {}".format(e))
+            logger.exception(e)
+
+    @staticmethod
+    def write_vocab_file(vocab: List[Tuple[Text, int]],
+                         output_path: Text) -> None:
+        try:
+            # write to file sorted by frequency
             v = sorted(vocab.items(), key=lambda x: x[1], reverse=True)
             with open(output_path, 'w') as f:
                 for tup in v:
@@ -37,24 +62,14 @@ class pyBPE:
             logger.exception(e)
 
     @staticmethod
-    def create_bpe_file(text: Text, n_codes: int, output_path: Text) -> None:
+    def write_codes_file(codes: List[Tuple[Text, Text, int]],
+                         output_path: Text) -> None:
         try:
-            # learn bpe codes
-            codes = bpe.learn_bpes(n_codes, text)
-
-            # to write to file
+            # write to file
             with open(output_path, 'w') as f:
                 for tup in codes:
                     f.write("{} {} {}\n".format(*tup))
 
         except Exception as e:
             logger.error("Unknown error while creating BPE codes file: {}".format(e))
-            logger.exception(e)
-
-    @staticmethod
-    def apply_bpe(text: Text, codes_path: Text, vocab_path: Text) -> Text:
-        try:
-            return bpe.apply_bpes(text, codes_path, vocab_path)
-        except Exception as e:
-            logger.error("Unknown error while applying BPE codes file: {}".format(e))
             logger.exception(e)
